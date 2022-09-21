a404024c2e8d7b52d00908ee820b6c69ec897c2f
KadGaurav <gauravkad139@gmail.com>
2022-09-21 14:55:50 +0530

    Complete Expense Tracker

--- expense-tracker/src/components/Expenses/ExpenseItem.jsx
+++ expense-tracker/src/components/Expenses/ExpenseItem.jsx
@@ -1,15 +1,17 @@
-import Cards from './Cards';
+import Cards from '../UI/Cards';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+   
     
     return(
         <Cards className='expense-item'>
             <ExpenseDate date = {props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item__price'>₹{props.amount}</div>
+                <div className='expense-item__price'>₹ {props.amount}</div>
             </div>
         </Cards>
     );
